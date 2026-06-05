972ab4f7d6c4c5153d3c5bf4abe0fbb7769cf702
daviparma101-sudo <davi.parma101@gmail.com>
2026-06-05 17:16:01 -0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,39 +1,37 @@
 from settings import settings
 import discord
 from bot_logic import *
-from comandos import comandos
+from discord.ext import commands
+from comandos import *
 
 # A variável intents armazena as permissões do bot
 intents = discord.Intents.default()
 # Ativar a permissão para ler o conteúdo das mensagens
 intents.message_content = True
 # Criar um bot e passar as permissões
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Fizemos login como {client.user}')
+    print(f'Fizemos login como {bot.user}')
+@bot.command()
+async def ola(ctx):
+    await ctx.reply("Olá!")
 
-@client.event
-async def on_message(message):
-    if not message.content.startswith('$') and not message.content.startswith('oi'):
-        return
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hello!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$pass'):
-        senha = gen_pass(12)
-        await message.channel.send(f"Sua senha é: `{senha}`")
-    elif message.content.startswith("$comandos"):
-        await message.channel.send("Esses são os comandos existentes:" + str(comandos))
-    elif message.content.startswith("oi"):
-        await message.channel.send("Olá, Como posso ajudar? Caso queira saber os comandos digite: $comandos")
-    else:
-        await message.channel.send("Não entendo esse comando!")
+@bot.command()
+async def moeda(ctx):
+    resultado = flip_coin()
+    await ctx.reply(resultado)
 
-client.run(settings["TOKEN"])
+@bot.command()
+async def senha(ctx, tamanho):
+    senha_gerada = gen_pass(int(tamanho))
+    await ctx.reply(senha_gerada) 
+
+@bot.command()
+async def comandos(ctx):
+    comandos = ["/senha (numero), /moeda, /ola, /comandos"]
+    await ctx.reply(f"os comandos são {comandos}")
+
+
+bot.run(settings["TOKEN"]) 
